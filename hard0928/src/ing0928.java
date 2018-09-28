8e5200a32f515ccf5424ab6400d34988c2b43819
Nicoleyu <43199808+NicoleyuC@users.noreply.github.com>
2018-09-29 00:41:25 +0800

    Add files via upload

--- hard0928/src/ing0928.java
+++ hard0928/src/ing0928.java
@@ -1,6 +1,15 @@
 public class ing0928 {
 
     public static void main(String[] args) {
-	// write your code here
+	    fun();
+    }
+    public static void fun(){
+        int n = 6;
+        for(int i = 0; i < n ; i++ ){
+            for( int j = 0 ; j <= i ; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
