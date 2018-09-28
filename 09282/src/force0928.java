a55c8d934ffc682e1412d926922c9735118612ae
Nicoleyu <43199808+NicoleyuC@users.noreply.github.com>
2018-09-29 01:27:11 +0800

    Add files via upload

--- 09282/src/force0928.java
+++ 09282/src/force0928.java
@@ -1,6 +1,12 @@
 public class force0928 {
 
     public static void main(String[] args) {
-	//Byte max value is
+	    int [ ][ ] data = new int [5][3];
+	    for (int i = 0 ; i < 5 ; i++ ){
+	        for(int j = 0 ; j < 3 ; j++ ){
+                System.out.print("data[" + i + "]" + "[" + j + "] = " +  data[i][j] + "\t" );
+            }
+            System.out.println();
+        }
     }
 }
